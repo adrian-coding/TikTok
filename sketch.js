75ea4f105a7161e329c7d164a29735bb917b30e9
adrian-coding <70213677+adrian-coding@users.noreply.github.com>
2020-12-03 12:31:31 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,16 +23,26 @@ function draw() {
   mnAngle = map(mn, 0, 60, 0, 360);
   scAngle = map(sc, 0, 60, 0, 360);
 
+
+
+push();
+  rotate(scAngle);
   stroke(255,0,0);
   strokeWeight(7);
-  line(400,200,450,150);
-  stroke(0,255,0);
+  line(0,0,50,0);
+  pop();
+  push();
+  rotate(minAngle);
+  stroke(255,0,0);
+  strokeWeight(7); 
+  line(0,0,75,0);
+  pop();
+  push();
+  rotate(hrAngle);
+  stroke(255,0,0);
   strokeWeight(7);
-  line(400,200,480,220);
-  stroke(0,0,255);
-  strokeWeight(7);
-  line(400,200,330,180);
-
+  line(0,0,100,0);
+  pop();
 
 
   stroke(255,0,255);
@@ -42,7 +52,7 @@ function draw() {
   //Seconds stroke(255,0,0);
   arc(0,0,300,300,0,scAngle); 
   //Minutes stroke(0,255,0);
-  arc(0,0,280,280,0,minAngle);
+  arc(0,0,280,280,0,mnAngle);
   //Hour stroke(0,0,255);
   arc(0,0,260,260,0,hrAngle);
 
